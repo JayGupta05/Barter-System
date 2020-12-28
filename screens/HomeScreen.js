a135258bd43b87435d3812e323fd812922e9f1e3
Jay Nishant Gupta <guptaj2895@mayoboys.com>
2020-12-28 20:27:54 +0530

    My Sixth Commit for 79

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import * as React from 'react-native';
+import * as React from 'react';
 import {View, Text, TouchableOpacity,StyleSheet, FlatList, Alert } from 'react-native';
 import db from '../config';
 import firebase from 'firebase';
@@ -11,6 +11,10 @@ export default class HomeScreen extends React.Component{
             allExchanges:[],
         }
         this.exchangeRef=null;
+    }
+
+    componentDidMount=()=>{
+        this.getAllExchanges();
     }
 
     getAllExchanges=()=>{
